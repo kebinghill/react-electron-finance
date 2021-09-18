b06c424f2866870d9bb0326ff200fa43a4f7f02c
Kevin Gil <93kevingil@gmail.com>
2021-09-17 21:57:43 -0400

    fixed text area for financial notes 50%

--- src/FinancialNotes.jsx
+++ src/FinancialNotes.jsx
@@ -9,7 +9,7 @@ export default function FinancialNotes() {
       <h3>Financial Notes</h3>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={12} />
+          <Form.Control as="textarea" rows={6} id="financial-text-area" />
         </Form.Group>
       </Form>
       <h3>To-Do List</h3>
