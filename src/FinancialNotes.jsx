e1a674b2dd1e532e6b7bea29444a3818772264c4
Kevin Gil <93kevingil@gmail.com>
2021-09-17 21:39:48 -0400

    added Form textbox to Financial Notes Home View

--- src/FinancialNotes.jsx
+++ src/FinancialNotes.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import './FinancialNotes.css';
+import Form from 'react-bootstrap/form';
+
 export default function FinancialNotes() {
   return (
     <div id="financial-notes">
       <h3>Financial Notes</h3>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={12} />
+        </Form.Group>
+      </Form>
+      <h3>To-Do List</h3>
     </div>
   );
 }
--- src/SeparateExpenses.jsx
+++ src/SeparateExpenses.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './SeparateExpenses.css';
+
 export default function SeparateExpenses() {
   return (
     <div id="separate-expenses">
